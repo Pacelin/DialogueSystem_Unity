da39e48ed8bc32aea3bbc40e463c0987f3933cc5
Pacelin <Warfacer2020@gmail.com>
2023-05-17 00:49:21 +0300

    Project Created

---
+++ Dialogue System/Assets/Dialogue System/Scripts/Dialogue.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Create/Dialogue")]
+public class Dialogue : ScriptableObject
+{
+    
+}
